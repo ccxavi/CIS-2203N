ea30f91e9715b5c84f7612fa4dcb73d1b8fa6438
ccxavi <ccxavillarin@gmail.com>
2026-02-26 22:49:53 +0800

    init todo list

---
+++ todo_list/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace todo_list;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ todo_list/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace todo_list;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
